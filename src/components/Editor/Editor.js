17f66045c6e1172db6d5a7b2cada1b4b786c5a46
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2021-12-01 12:09:10 -0700

    Updates title and subtitle on input

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -2,15 +2,26 @@ import React from 'react';
 
 import './Editor.css';
 
-export default function Editor() {
+export default function Editor({
+  title,
+  setTitle,
+  subtitle,
+  setSubtitle,
+  font,
+  setFont,
+  align,
+  setAlign,
+  text,
+  setText,
+}) {
   return (
     <div className="editor">
       <div className="form-control">
-        <input name="title" type="text" />
+        <input value={title} name="title" type="text" onChange={(e) => setTitle(e.target.value)} />
         <label htmlFor="title">Title</label>
       </div>
       <div className="form-control">
-        <input type="text" />
+        <input value={subtitle} type="text" onChange={(e) => setSubtitle(e.target.value)} />
         <label>Subtitle</label>
       </div>
       <div className="form-control">
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { useState } from 'react';
 import Preview from '../../components/Preview/Preview';
 import Editor from '../../components/Editor/Editor';
 
@@ -7,12 +7,30 @@ import './Home.css';
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
+  const [title, setTitle] = useState('My Blog');
+  const [subtitle, setSubtitle] = useState('subtitle');
+  const [font, setFont] = useState('font');
+  const [align, setAlign] = useState('align');
+  const [text, setText] = useState('text');
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
-      <Editor />
+      <Preview {...{ title, subtitle, font, align, text }} />
+      <Editor
+        {...{
+          title,
+          setTitle,
+          subtitle,
+          setSubtitle,
+          font,
+          setFont,
+          align,
+          setAlign,
+          text,
+          setText,
+        }}
+      />
     </main>
   );
 }
